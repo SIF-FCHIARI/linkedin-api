fc8c528f56142bb05fbeec72746486034db92640
SIF-FCHIARI <116073730+SIF-FCHIARI@users.noreply.github.com>
2022-11-18 17:30:29 +0100

    Update linkedin.py

--- linkedin_api/linkedin.py
+++ linkedin_api/linkedin.py
@@ -404,8 +404,8 @@ class Linkedin(object):
         params = {"decorationId": "com.linkedin.voyager.dash.deco.search.SearchClusterCollection-169",
                   "origin": "FACETED_SEARCH",
                   "q": "all",
-                  "query":f"(keywords:{keywords},flagshipSearchIntent:SEARCH_SRP,queryParameters:(companyHqGeo:List({regions},), industryCompanyVertical:List({industries}),resultType:List(COMPANIES)),includeFiltersInResponse:false)"
-                  "start": "0",}
+                  "query":f"(keywords:{keywords},flagshipSearchIntent:SEARCH_SRP,queryParameters:(companyHqGeo:List({regions},), industryCompanyVertical:List({industries}),resultType:List(COMPANIES)),includeFiltersInResponse:false)",
+                  "start": "0"}
     
 
         data = self.search(params, **kwargs)
